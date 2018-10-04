5330f1707fe078ee7b810cfaf420732f0cc536d2
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-04 18:33:24 +0200

    Rework install script

--- install-all.sh
+++ install-all.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-ansible-playbook system/static-ip.yml -i "$1," --extra-vars "host=all ip=$2"
-ansible-playbook system/centos.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook system/docker.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook system/restic.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook system/traefik.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/alltube.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/cryptpad.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/etherpad.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/firefox_syncserver.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/lutim.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/privatebin.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/prosody.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/radicale.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/send.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/wallabag.yml -i inventory.yml --extra-vars "host=ao"
-ansible-playbook services/www.yml -i inventory.yml --extra-vars "host=ao"
+ansible-playbook system/static-ip.yml -i "$2," --extra-vars "host=all ip=$3"
+ansible-playbook system/centos.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook system/docker.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook system/restic.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook system/traefik.yml -i inventory.yml --extra-vars "host=$1"
+
+### SERVICES
+
+ansible-playbook services/alltube.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/cryptpad.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/etherpad.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/firefox_syncserver.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/lutim.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/privatebin.yml -i inventory.yml --extra-vars "host=$1"
+#ansible-playbook services/prosody.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/radicale.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/send.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/wallabag.yml -i inventory.yml --extra-vars "host=$1"
+
+### WEBSITES
+
+ansible-playbook services/www.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/elzire.fr.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/wordpress.yml -i inventory.yml --extra-vars "host=$1 wordpress_domain=moodevie.com"
