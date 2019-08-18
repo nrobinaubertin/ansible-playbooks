f5b358f31499588d0e191585f7cee3bf33f942d8
Niels Robin-Aubertin <git@niels.fr>
2019-08-18 17:36:19 +0200

    Rework order of activations

--- install-all.sh
+++ install-all.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
+### SYSTEM
+
 #ansible-playbook system/static-ip.yml -i "$2," --extra-vars "host=all ip=$3"
-#ansible-playbook system/centos.yml -i inventory.yml --extra-vars "host=$1"
-#ansible-playbook system/docker.yml -i inventory.yml --extra-vars "host=$1"
-#ansible-playbook system/restic.yml -i inventory.yml --extra-vars "host=$1"
-#ansible-playbook system/traefik.yml -i inventory.yml --extra-vars "host=$1"
+#ansible-playbook system/centos.yml -i $2 --extra-vars "host=$1"
+#ansible-playbook system/docker.yml -i $2 --extra-vars "host=$1"
+#ansible-playbook system/restic.yml -i $2 --extra-vars "host=$1"
 
-### SERVICES
-
-ansible-playbook services/alltube.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/cryptpad.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/etherpad.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/firefox_syncserver.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/glances.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/lutim.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/privatebin.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/radicale.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/send.yml -i inventory.yml --extra-vars "host=$1"
-ansible-playbook services/wallabag.yml -i inventory.yml --extra-vars "host=$1"
-
-### CLEAN
-ansible-playbook system/clean.yml -i inventory.yml --extra-vars "host=$1"
+ansible-playbook services/traefik.yml -i $2 --extra-vars "host=$1"
 
 ### WEBSITES
 
-#ansible-playbook services/www.yml -i inventory.yml --extra-vars "host=$1"
-#ansible-playbook services/elzire.fr.yml -i inventory.yml --extra-vars "host=$1"
-#ansible-playbook services/wordpress.yml -i inventory.yml --extra-vars "host=$1 wordpress_domain=moodevie.com"
+ansible-playbook services/www.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/elzire.fr.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/zus.am.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/wordpress.yml -i $2 --extra-vars "host=$1 wordpress_domain=moodevie.com"
+
+### SERVICES
+
+ansible-playbook services/alltube.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/cryptpad.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/etherpad.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/firefox_syncserver.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/glances.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/lutim.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/privatebin.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/radicale.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/send.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/wallabag.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/kanboard.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/prosody.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/freshrss.yml -i $2 --extra-vars "host=$1"
+ansible-playbook services/ut2004.yml -i $2 --extra-vars "host=$1"
+
+### CLEAN
+ansible-playbook system/clean.yml -i $2 --extra-vars "host=$1"
